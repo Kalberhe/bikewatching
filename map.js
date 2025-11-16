8c2d6ada5805152bec874e0bfae72f6ee0c4ae86
Kalberhe <kgebrekirstos@ucsd.edu>
2025-11-15 16:39:51 -0800

    Add Mapbox GL and map.js

---
+++ map.js
@@ -0,0 +1,5 @@
+// Import Mapbox as an ESM module
+import mapboxgl from "https://cdn.jsdelivr.net/npm/[email protected]/+esm";
+
+// Check that Mapbox GL JS is loaded
+console.log("Mapbox GL JS Loaded:", mapboxgl);
