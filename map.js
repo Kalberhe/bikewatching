9eab479bf263ab1055526d1895ff29e0f712c2ba
Kalberhe <kgebrekirstos@ucsd.edu>
2025-11-15 16:53:54 -0800

    map things

--- map.js
+++ map.js
@@ -1,5 +1,18 @@
-// Import Mapbox as an ESM module
 import mapboxgl from "https://cdn.jsdelivr.net/npm/[email protected]/+esm";
 
-// Check that Mapbox GL JS is loaded
 console.log("Mapbox GL JS Loaded:", mapboxgl);
+
+mapboxgl.accessToken = "pk.eyJ1Ijoia2FsYmVyaGUiLCJhIjoiY21pMHp5a2Z1MTVmMDJ3cTY0MWVqeThhMiJ9.ouuEXzVnn7gEK3uWa6pWLw";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: [-71.09415, 42.36027], 
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
+
+map.on("load", () => {
+  console.log("Map has loaded!");
+});
